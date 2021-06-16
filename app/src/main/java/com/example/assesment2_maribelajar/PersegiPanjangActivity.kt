280c20d2818f310e1af5a6830ac0716a1ac28b51
RizqIhza27 <rihza27@gmail.com>
2021-06-16 17:43:02 +0700

    finish activity

---
+++ app/src/main/java/com/example/assesment2_maribelajar/PersegiPanjangActivity.kt
@@ -0,0 +1,11 @@
+package com.example.assesment2_maribelajar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PersegiPanjangActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_persegi_panjang)
+    }
+}
